8b851850a6d9125d3869ca86178153bdee85cd2a
inecsiv <165045449+inecsiv@users.noreply.github.com>
2024-03-27 02:00:13 -0700

    Update admin-login.js

--- admin-login.js
+++ admin-login.js
@@ -4,7 +4,7 @@ document.getElementById("admin-login-form").addEventListener("submit", function(
     var adminPassword = document.getElementById("admin-password").value;
     
     // Check if admin ID and password are correct (you can replace this with your own authentication logic)
-    if (adminId === "admin" && adminPassword === "password") {
+    if (adminId === "ssaikia" && adminPassword === "saikia7896") {
       // Redirect to admin panel page after successful login
       window.location.href = "admin-panel.html"; // Change the URL to your admin panel page
     } else {
