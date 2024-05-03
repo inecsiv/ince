e30dd606411f905014d236ae61f8cc49dd49b2f1
inecsiv <165045449+inecsiv@users.noreply.github.com>
2024-05-03 18:18:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,3 @@ function toggleMenu() {
     var menu = document.getElementById('menu');
     menu.classList.toggle('active');
 }
-
-$(document).bind("contextmenu",function(e) {
- e.preventDefault();
-});
