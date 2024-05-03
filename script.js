632af8c9fca53c482d1cf29309d37ed9ecd8d795
inecsiv <165045449+inecsiv@users.noreply.github.com>
2024-05-03 18:24:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ function toggleMenu() {
     var menu = document.getElementById('menu');
     menu.classList.toggle('active');
 }
+
+
+/* Disable F12 key press */
+window.addEventListener('keydown', function (event) {
+    if (event.keyCode === 123) { 
+        console.log('F12 key was pressed.'); 
+        event.preventDefault(); 
+        return false;
+    }
+  });
+  
+  
+  /* Disable Ctrl+U (View Page Source) */
+  document.onkeydown = function (event) {
+    event = event || window.event;
+    if (event.ctrlKey && event.keyCode == 85) { 
+        return false;
+    }
+  };
